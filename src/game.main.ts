b37c017329bd69e57331044d49af6ed6df19407b
James Sral <jamessral@gmail.com>
2018-03-06 19:46:24 -0500

    Fix stubbed game file

--- src/game.main.ts
+++ src/game.main.ts
@@ -1,7 +1,31 @@
-import Phaser from 'phaser';
+import 'phaser';
 
-class Game extends Phaser.Game {}
+interface IConfig {
+  width: number;
+  height: number;
+  type?: number;
+  parent?: string;
+  physics?: any;
+  scene?: [Phaser.Scene];
+}
+
+class Game extends Phaser.Game {
+  constructor(config: IConfig) {
+    super(config);
+  }
+}
 
 window.onload = () => {
-  const game = new Game();
+  const game = new Game({
+    height: 600,
+    parent: 'game',
+    physics: {
+      arcade: {
+        gravity: { y: 250 },
+      },
+      default: 'arcade',
+    },
+    type: Phaser.AUTO,
+    width: 800,
+  });
 };
